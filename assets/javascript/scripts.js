070df6f29a29ee0148bfe1ca4ee19693caefc2a2
ChiefDNA <ngabiranodaniel74@gmail.com>
2025-04-24 18:06:52 +0300

    Update scripts.js

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,15 +1,12 @@
-console.log("Script loaded"); 
 document.addEventListener("DOMContentLoaded",()=>{
-    source = document.URL + 'assets/javascript/content.json'
-    console.log(source);
-    fetch(source)
+//     source = document.URL + 'assets/javascript/content.json'
+    fetch('assets/javascript/content.js')
     .then(response => response.json())
     .then(data =>{
-        console.log("succesfully called the file",data)
         data.forEach(item => {
             document.querySelector('#paragraphs').innerHTML += `<div class="parag"><p>${item.content}</p></div>`
         });
     })
-    .catch(error => console.log('Error:', error));
+    .catch(error => console.error('Error:', error));
 });
 
