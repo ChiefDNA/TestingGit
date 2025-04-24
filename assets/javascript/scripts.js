953931292d0f0aa1cc34d515dae3e7cedbf8bf82
ChiefDNA <ngabiranodaniel74@gmail.com>
2025-04-24 17:04:49 +0300

    Update scripts.js

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    counter = 1;
-    fetch('/TestingGit/assets/javascript/content.json')
+    source = document.URL + 'assets/javascript/content.json'
+    fetch(source)
     .then(response => response.json())
     .then(data =>{
+        console.log("succesfully called the file",data)
         data.forEach(item => {
             document.querySelector('#paragraphs').innerHTML += `<div class="parag"><p>${item.content}</p></div>`
             counter++;
