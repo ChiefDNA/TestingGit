b91b7932161acb8be46a8c7a94a5cd03e0edc9aa
ChiefDNA <ngabiranodaniel74@gmail.com>
2025-04-24 21:14:04 +0300

    fixed script tag and added document type

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    counter = 1;
-    fetch('assets/javascript/content.json')
+    const source = new URL('assets/javascript/content.json', document.URL);
+    fetch(source)
     .then(response => response.json())
     .then(data =>{
+        console.log("succesfully proccesed data");
         data.forEach(item => {
             document.querySelector('#paragraphs').innerHTML += `<div class="parag"><p>${item.content}</p></div>`
-            counter++;
         });
     })
     .catch(error => console.error('Error:', error));
-})
+});
