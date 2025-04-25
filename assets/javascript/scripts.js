6a505db7606b93cf8f3b354639dab21beb04713b
ChiefDNA <ngabiranodaniel74@gmail.com>
2025-04-25 11:42:34 +0300

    linked login to index and input wrnings

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,30 +1,57 @@
 document.addEventListener("DOMContentLoaded",()=>{
     
-  document.querySelector('#username').addEventListener('input',replacement);
-  document.querySelector('#password').addEventListener('input',replacement);
-  const form =document.querySelector('#loginform');
-  form.addEventListener('submit',(e)=>{
-    e.preventDefault();
+  const userfield = document.querySelector('#username');
+  if (userfield){
 
-    const username = document.querySelector('#username').value.trim();
-    const password = document.querySelector('#password').value.trim();
-    const remember = form.querySelector('input[type="checkbox"]').checked;
+    const passwordField = document.querySelector('#password');
+    const form =document.querySelector('#loginform');
 
-    if (username==="admin" && password==="admin"){
-        alert("Login Successful!");
-        if(remember){
-            localStorage.setItem("username",username);
-        }
-        window.location.href = "index.html";
-    }
-    else {
-        alert("invalid username or password.");
-    }
-  })  
+    document.querySelector(".section").addEventListener("click",()=>{window.location.href = "index.html";})
+    document.querySelectorAll(".form-boundary, .row").forEach(element =>{
+      element.addEventListener("click",(e)=>e.stopPropagation());
+    });
+
+    passwordField.addEventListener('input',replacement);
+    userfield.addEventListener('input',replacement);
+
+    form.addEventListener('submit',(e)=>{
+      e.preventDefault();
+
+      const username = userfield.value.trim();
+      const password = passwordField.value.trim();
+      const remember = form.querySelector('input[type="checkbox"]').checked;
+
+      if (username==="admin" && password==="admin"){
+          alert("Login Successful!");
+          if(remember){
+              localStorage.setItem("username",username);
+          }
+          window.location.href = "index.html";
+      }
+      else {
+          alert("invalid username or password.");
+      }
+    });
+  }  
 });
 
 function replacement(event){
     const input = event.target;
-    input.value = input.value.replace(/[^a-zA-Z0-9]/g,'');
+    input.value = input.value.replace(/[^a-zA-Z0-9]/g,()=>UserAlert(input));
 
+}
+function UserAlert( input){
+  if (!input.nextElementSibling || !input.nextElementSibling.classList.contains("char-warning")){
+    const message = document.createElement("div");
+    message.textContent ="AlphaNumeric characters Only";
+    message.className = "char-warning";
+    input.parentElement.appendChild(message);
+
+    setTimeout(()=>{
+      if (message && message.parentElement){
+        message.remove();
+      }
+    },2000);
+  }
+  return "";
 }
